926cc7a6bb2b039ae059a41a4f22bf14c64fece1
Joshua Hein <josh.hein@EVAPCO.COM>
2021-01-20 12:35:03 -0500

    Add Post route for minions

--- server/api.js
+++ server/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const apiRouter = express.Router();
 const db = require('./db');
 
+// Minion Routes
 apiRouter.param('minionId', (req, res, next, id) => {
     if (Number.parseInt(id)) {
         if (db.getFromDatabaseById('minions', id)) {
@@ -23,6 +24,16 @@ apiRouter.get('/minions', (req, res, next) => {
 apiRouter.get('/minions/:minionId', (req, res, next) => {
     const minion = db.getFromDatabaseById('minions', req.minionId);
     res.send(minion);
+});
+
+apiRouter.post('/minions', (req, res, next) => {
+    const minion = db.addToDatabase('minions', req.body);
+    res.status(201).send(minion);
+});
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+    const minion = db.getFromDatabaseById('minions', req.minionId);
+
 });
 
 module.exports = apiRouter;
